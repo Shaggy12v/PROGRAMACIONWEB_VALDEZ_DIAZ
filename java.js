9de454040841e098419e7a83b4c1a428cb9e7526
Shaggy12v <87054003+Shaggy12v@users.noreply.github.com>
2021-07-06 22:00:50 -0700

    AYUDA

---
+++ java.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.section').forEach(section => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: 'top top',
+    pin: true,
+    pinSpacing: false
+  });
+});
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const imgLightBox = document.querySelectorAll('.materialboxed');
+	M.Materialbox.init(imgLightBox, {
+		inDuration: 500,
+		outDuration: 500
+	});
+});
